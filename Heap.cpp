44ad315cec1e69e94d5edddf59698ca1627a663e
tgdh91 <37333340+tgdh91@users.noreply.github.com>
2018-05-26 10:02:30 +0800

    初步完成

--- Heap.cpp
+++ Heap.cpp
@@ -1,27 +1,4 @@
-#include <vector>
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
-
-using namespace std;
-
-class Database	//整体数据结构
-{
-public:
-	Database();
-	Database(const char*);
-	~Database();
-	int Get_Row();
-	int Get_Col(int);
-	char* Get_Line(int);
-	char* Data_Get(unsigned int, unsigned int, unsigned int);
-	void Data_Replace(unsigned int, unsigned int, const char*, unsigned int, unsigned int);
-	void File_Save(const char*);
-
-private:
-	vector <vector <char*> > data;	//二维容器
-	vector <int> line_num;//记录每行字符个数，0号结点记录行数
-};
+#include "Heap.h"
 
 Database::Database()
 {
@@ -107,26 +84,167 @@ int Database::Get_Col(int row)	//返回第row行字符数
 
 char* Database::Get_Line(int row)
 {
-	char* line = new char(line_num[row] + 2);
-	int col;
+	char* line = new char(line_num[row] + 1);
+	int i;
 
-	for (col = 1; col <= line_num[row]; col++)
+	for (i = 0; i < line_num[row]; i++)
 	{
-		line[col] = data[row][(col - 1) / 100 + 1][(col - 1) % 100 + 1];
+		line[i] = data[row][(i) / 100 + 1][(i) % 100 + 1];
 	}
 
 	return line;
 
 }
 
-char* Data_Get(unsigned int row, unsigned int col, unsigned int n)
+char* Database::Data_Get(unsigned int row, unsigned int col, unsigned int n)
 //从第row行的第col个字符开始获得的长度为n的字符串
 {
+	char* line = new char(n + 1);
+
+	for (int i = 0; col <= line_num[row]; col++, i++)
+	{
+		line[0] = data[row][(col - 1) / 100 + 1][(col - 1) % 100 + 1];
+	}
+
+	return line;
+}
+
+void Database::Data_Replace(unsigned int row, unsigned int col, const char* new_ch, unsigned int old_len, unsigned int new_len)
+//从第row行的第col个字符开始将长度为old_len的字符串替换为长度为new_len的字符串new_ch
+{
+	char *temp = NULL;
+	int i, t;
+	int intercept = old_len - new_len;
+	temp = new char(line_num[row] - col - old_len + 10);
+	if (intercept == 0)
+	{
+		for (i = 0; i < old_len; i++, col++)
+		{
+			data[row][(col - 1) / 100 + 1][(col - 1) % 100 + 1] = new_ch[i];
+		}
+	}
+	else
+	{
+		t = 0;
+		if (abs(intercept) % 8 != 0)
+		{
+			char c;
+			for (i = 0; col + old_len + i <= line_num[row]; i++)
+			{
+				c = data[row][(col + old_len + i - 1) / 100 + 1][(col + old_len + i - 1) % 100 + 1];
+				if (t == 0 && (c == '\0' || c == '\t'))
+				{
+					int old_zero = 8 - (col + old_len + i) % 8;
+					int new_zero = 8 - (col + old_len + i - intercept) % 8;
+					t = old_zero - new_zero;
+					if (t > 0)
+						temp[i] = data[row][(col + old_len + i + t - 1) / 100 + 1][(col + old_len + i + t - 1) % 100 + 1];
+					else if (t < 0)
+						temp[i + t] = c;
+				}
+				else
+				{
+					if (t >= 0)
+						temp[i] = data[row][(col + old_len + i + t - 1) / 100 + 1][(col + old_len + i + t - 1) % 100 + 1];
+					else
+						temp[i + t] = c;
+				}
+
+			}
+		}
+		else
+		{
+			for(i = 0; col + old_len + i <= line_num[row]; i++)
+				temp[i] = data[row][(col + old_len + i - 1) / 100 + 1][(col + old_len + i - 1) % 100 + 1];
+		}
+
+		if (intercept > 0 && line_num[row] % 100 <= intercept + t)
+		{
+			data[row].pop_back();
+		}
+		else if(intercept  < 0 && (100 - line_num[row] % 100) < -intercept - t)
+		{
+			data[row].push_back(new char[102]());
+		}
+		
+		line_num[row] = line_num[row] - intercept - t;
+		
+		for (i = 0; i < new_len; i++)
+			data[row][(col + i - 1) / 100 + 1][(col + i - 1) % 100 + 1] = new_ch[i];
+		for (i = 0; i < line_num[row] - col - new_len + 1; i++)
+		{
+			data[row][(col + new_len + i - 1) / 100 + 1][(col + new_len + i - 1) % 100 + 1] = temp[i];
+		}
+	}
+	free(temp);
+}
+
+void Database::Data_Insert(unsigned int row, unsigned int col, const char c)
+{
+	if (c == '\t')
+	{
+		int zero = 8 - (col % 8);
+		char *temp = new char(zero + 2);
+		temp[zero] = '\t';
+		Data_Replace(row, col, temp, 0, zero + 1);
+		free(temp);
+	}
+	else if (c == '\n')
+	{
+		char *temp = new char(line_num[row] - col + 2);
+		int len = line_num[row] - col + 1;
+		for (int i = col; i <= line_num[row]; i++)
+			temp[i - col] = data[row][(i - 1) / 100 + 1][(i - 1) % 100 + 1];
+		Data_Replace(row, col, temp, len, 0);
+		Data_Replace(row, col, "\n", 0, 1);
+		vector< vector<char*> >::iterator it1 = data.begin() + row + 1;
+		vector <char*> _data;
+		data.insert(it1, _data);
+		line_num[0]++;
+		vector<int>::iterator it2 = line_num.begin() + row + 1;
+		line_num.insert(it2, 0);
+		data[row + 1].push_back(NULL);
+		data[row + 1].push_back(new char[102]());
+		data[row + 1][1][1] = '\n';
+		line_num[row + 1] = 1;
+		Data_Replace(row + 1, 1, temp, 1, len);
+		free(temp);
+	}
+	else
+	{
+		char a[2];
+		a[0] = c;
+		Data_Replace(row, col, a, 0, 1);
+	}
 
 }
 
-void Data_Replace(unsigned int row, unsigned int col, const char* new_ch, unsigned int old_len, unsigned int new_len);
-//从第row行的第col个字符开始将长度为old_len的字符串替换为长度为new_len的字符串new_ch
+void Database::Data_Delete(unsigned int row, unsigned int col)
+{
+	if (col == 0)
+	{
+		char *temp = Get_Line(row);
+		Data_Replace(row - 1, line_num[row - 1], temp, 1, line_num[row]);
+		vector< vector<char*> >::iterator it1 = data.begin() + row;
+		data.erase(it1);
+		vector<int>::iterator it2 = line_num.begin() + row;
+		line_num.erase(it2);
+		line_num[0]--;
+
+	}
+	else
+	{
+		char c = data[row][(col - 1) / 100 + 1][(col - 1) % 100 + 1];
+		if (c == '\t')
+		{
+			int i;
+			for (i = 1; data[row][(col - i - 1) / 100 + 1][(col - i - 1) % 100 + 1] == 0; i++) {}
+			Data_Replace(row, col - i + 1, "", i, 0);
+		}
+		else
+			Data_Replace(row, col, "", 1, 0);
+	}
+}
 
 void Database::File_Save(const char* filename)
 {
@@ -146,6 +264,38 @@ void Database::File_Save(const char* filename)
 	}
 }
 
+int Database::Line_Check(int row, int col)
+{
+	char c = data[row][(col - 1) / 100 + 1][(col - 1) % 100 + 1];
+	if (col <= line_num[row] && c != '\n' && c != '\0' && c != '\t')
+		return 0;
+	else if (row > line_num[0])
+		return 1;
+	else if (col - line_num[row] + 1 > EDITWIDTH - ((line_num[row] - 1) % EDITWIDTH))
+		return 2;
+	else if (col > line_num[row] - 1)
+		return -(col - line_num[row] + 1);
+	else
+	{
+		if (col == 8)
+			return -1;
+		else
+		{
+			int i;
+			for (i = 1; data[row][(col - i - 1) / 100 + 1][(col - i - 1) % 100 + 1] != '\0') {}
+			return -i;
+		}
+	}
+}
 Database::~Database()
 {
+}
+
+int main()
+{
+	Database A("123.txt");
+	A.Data_Delete(2, 0);
+	//A.Data_Replace(1, 4, "456", 1, 3);
+	A.File_Save("321.txt");
+	return 0;
 }
